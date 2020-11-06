5175623218ca99a98b17c30f16eb775dc7a9811e
Satyanarayana <andeysatyanarayanamca@gmail.com>
2020-11-06 18:13:59 +0530

    commit with List Controller UIDesign and functionality

---
+++ ListApp/ListApp/HelperClasses/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ListApp
+//
+//  Created by Satya on 06/11/20.
+//  Copyright © 2020 Satya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let BASE_URL = ""
+let MAX_WIDTH = UIScreen.main.bounds.width <= 375 ? UIScreen.main.bounds.width : 375
+let FONT_SIZE_TITLE_LABEL = 15 / 320 * MAX_WIDTH
+let FONT_SIZE_DESCRIPTION_LABEL = 13 / 320 * MAX_WIDTH
---
+++ ListApp/ListApp/HelperClasses/Utility.swift
@@ -0,0 +1,15 @@
+//
+//  Utility.swift
+//  ListApp
+//
+//  Created by Satya on 06/11/20.
+//  Copyright © 2020 Satya. All rights reserved.
+//
+
+import Foundation
+import Network
+import CoreFoundation
+class Utility: NSObject {
+    static var sharedInstance = Utility()
+    var isReachable = false
+}
